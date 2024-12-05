8be230e8ec3b42d0164815a88373b520197c85bd
Davdi <david.gildinger07@htl-salzburg.ac.at>
2024-12-05 09:26:00 +0100

    inital commit

--- Assets/MyGame/Scripts/RoomManager.cs
+++ Assets/MyGame/Scripts/RoomManager.cs
@@ -14,20 +14,26 @@ public class RoomManager : MonoBehaviour
         {
             for (int col = 0; col < tableLayout.columns; col++)
             {
-                Vector3 tablePosition = new Vector3(col * tableLayout.tableSpacing, 0, row * 
+                Vector3 tablePosition = new Vector3(col * tableLayout.tableSpacing, 0, row *
                   tableLayout.tableSpacing);
-                
+
                 //Tische platzieren
                 GameObject table = Instantiate(tablePrefab, tablePosition, Quaternion.identity, transform);
 
-                //Sesselplatzieren
-            }            
+                // Sessel platzieren
+                Transform pos1 = table.transform.Find("ChairPos1");
+                Transform pos2 = table.transform.Find("ChairPos2");
+
+                if (pos1)
+                {
+                    Instantiate(chairPrefab, pos1.position, pos1.rotation, table.transform);
+                }
+
+                if (pos2)
+                {
+                    Instantiate(chairPrefab, pos2.position, pos2.rotation, table.transform);
+                }
+            }
         }
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
     }
 }
